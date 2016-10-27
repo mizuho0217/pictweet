2b0704067dc47e0d5b2641c973f8539d520dc00b
mizuho0217 <mizurinrinrin3@yahoo.co.jp>
2016-10-27 19:59:21 +0900

    Tweetsテーブルからname欄を削除した

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161027075001) do
+ActiveRecord::Schema.define(version: 20161027105507) do
 
   create_table "tweets", force: :cascade do |t|
     t.string   "name",       limit: 255
